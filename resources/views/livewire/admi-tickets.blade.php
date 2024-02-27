41f3ffd3eb0b02154f72dabbba5619273e976086
Diego DOminguez Z <software.fe@grupoccima.com>
2024-02-27 16:33:10 -0600

    Primeros Cambios en el Proyecto

---
+++ resources/views/livewire/admi-tickets.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Hola Mundo</h1>
+</div>
---
+++ routes/admi.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\HomeController;
+
+Route::get('',[HomeController::class, 'index']);
+
+
